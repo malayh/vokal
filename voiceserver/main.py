73db2e46ec2c246b084af0881caa199c80fe9db5
Malay <malay.hazarika@gmail.com>
2021-04-06 23:20:19 +0530

    Init voice server

--- voiceserver/main.py
+++ voiceserver/main.py
@@ -1,6 +1,36 @@
 import aiortc
 from tinyMQ import Producer, Consumer
+from typing import Set, Dict, List
+import logging
+import multiprocessing
 
+TMQ_HOST = "localhost"
+TMQ_PORT = 9800
+TOPIC_FROM_ROOT_SERVER = "fromRootServer"
+TOPIC_TO_ROOT_SERVER = "fromVoiceServer"
+
+
+class Room:
+    def __init__(self, id: int, q : multiprocessing.connection.Connection):
+        self.id = id
+        self.queue : multiprocessing.connection.Connection = q
+
+
+class LiveRoom:
+    def __init__(self):
+        self.outQ = Producer(TMQ_HOST,TMQ_PORT,TOPIC_TO_ROOT_SERVER)
 
 class VoiceServer:
-    pass
+    def __init__(self):
+        self.active_rooms : Dict[int, Room] = dict()
+        self.inQ = Consumer(TMQ_HOST,TMQ_PORT,TOPIC_FROM_ROOT_SERVER)
+
+    async def poll_forever(self):
+        pass
+
+    async def start(self):
+        await self.inQ.init_conn()
+        await self.poll_forever()
+
+
+
